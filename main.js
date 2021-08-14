3da5d3abebb4e569a9fe13087f0b637f32d4bc21
Oya-Tomo <82521012+Oya-Tomo@users.noreply.github.com>
2021-08-14 18:38:06 +0900

    add saving window size

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const electron = require('electron');
+const Store = require('electron-store');
 const app = electron.app;
 const ipcMain = electron.ipcMain;
 const BrowserWindow = electron.BrowserWindow;
 
 let mainWindow = null;
 
+const store = new Store();
 
 app.on('window-all-closed', function() {
     if (process.platform !== 'darwin') {
@@ -13,9 +15,13 @@ app.on('window-all-closed', function() {
 });
 
 app.on('ready', function() {
+    var width = store.get('width') || 800;
+    var height = store.get('height') || 500;
+    var isMax = store.get('isMax') || false;
+  // ブラウザ(Chromium)の起動, 初期画面のロード
     mainWindow = new BrowserWindow({
-        width: 800,
-        height: 500,
+        width: width,
+        height: height,
         frame: false,
         webPreferences: {
             nodeIntegration: false,
@@ -24,6 +30,21 @@ app.on('ready', function() {
         }
     });
     mainWindow.loadURL('file://' + __dirname + '/main.html');
+
+    if (isMax) {
+        mainWindow.maximize();
+    }
+
+    mainWindow.on('resize', function() {
+        if (mainWindow.isMaximized()) {
+            store.set("isMax", true);
+        } else {
+            store.set("isMax", false);
+            const size = mainWindow.getSize();
+            store.set("width", size[0]);
+            store.set("height", size[1]);
+        }
+    });
 
     mainWindow.on('closed', function() {
         mainWindow = null;
